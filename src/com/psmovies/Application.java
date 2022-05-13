89334f5528c24765ca2372efb6d2692e883f83d9
Daniel Silva <daniel.mascr@gmail.com>
2022-05-13 09:01:40 -0300

    First refactoring

--- src/com/psmovies/Application.java
+++ src/com/psmovies/Application.java
@@ -4,29 +4,24 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 
 public class Application {
-
-	private static void getMovies(String uri) throws Exception {
-
-		HttpClient client = HttpClient.newHttpClient();
-		
-		HttpRequest request = HttpRequest.newBuilder()
-			.uri(URI.create(uri))
-			.build();
-		
-		HttpResponse<String> response = 
-			client.send(request, BodyHandlers.ofString());
-		
-		System.out.println(response.body());
-		System.out.printf("Status code: %d", response.statusCode());
-
-	}
 	
 	public static void main(String[] args) throws Exception {
 		
-		getMovies("https://imdb-api.com/en/API/Top250Movies/<apiKey>");
+		String apiKey = "<apiKey>";
+		URI apiIMDB = URI.create("https://imdb-api.com/en/API/Top250Movies/" + apiKey);
+
+		HttpClient client = HttpClient.newHttpClient();		
+		HttpRequest request = HttpRequest.newBuilder().uri(apiIMDB).build();
+		
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		
+		Integer statusCode = response.statusCode();
+		String json = response.body();
+		
+		System.out.println("Status code: " + statusCode);
+		System.out.println("Answer: " + json);
 		
 	}
 
