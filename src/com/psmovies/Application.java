a85ccd6be34f3e0c6c3cb1cac7e4c8b912d21e37
Daniel Silva <daniel.mascr@gmail.com>
2022-05-16 21:06:57 -0300

    Response parse json and attributes extraction

--- src/com/psmovies/Application.java
+++ src/com/psmovies/Application.java
@@ -4,6 +4,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
 	
@@ -17,12 +21,44 @@ public class Application {
 		
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 		
-		Integer statusCode = response.statusCode();
 		String json = response.body();
 		
-		System.out.println("Status code: " + statusCode);
-		System.out.println("Answer: " + json);
+		Pattern pattern = Pattern.compile(".*\\[(.*)\\].*");
+		Matcher matcher = pattern.matcher(json);
 		
+		matcher.matches();
+		
+		String[] objects = matcher.group(1).split("\\},\\{");
+		
+		for (int i = 0; i < objects.length; i++) {
+			objects[i] = objects[i].replace("{", "").replace("}", "");
+		}
+		
+		List<String> titles = Arrays.asList(parseTitles(objects));		
+		titles.forEach(System.out::println);
+		
+		System.out.println();
+		
+		List<String> imgUrls = Arrays.asList(parseUrlImages(objects));
+		imgUrls.forEach(System.out::println);
+
+	}
+	
+	public static String[] attributes(String[] moviesArray, int attributePositionInArray, int attributeInitialPosition) {
+		String[] attributesArray = new String[moviesArray.length];
+		for (int i = 0; i < moviesArray.length; i++) {
+			String[] movie = moviesArray[i].split(",");
+			attributesArray[i] = movie[attributePositionInArray].substring(attributeInitialPosition, movie[attributePositionInArray].length() - 1);
+		}
+		return attributesArray;
+	}
+	
+	public static String[] parseTitles(String[] moviesArray) {
+		return attributes(moviesArray, 2, 9);
+	}
+
+	public static String[] parseUrlImages(String[] moviesArray) {
+		return attributes(moviesArray, 5, 9);
 	}
 
 }
